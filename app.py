30759ec6e85b1596636442589c67cd20db51fff8
Blair Hagen <bhagen55@gmail.com>
2018-08-16 13:50:09 -0700

    pep8 all the things

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template
 from hl1voxsentenceformer import hl1sentenceformer as vox
-from flask_simplelogin import SimpleLogin, login_required
 
 
 app = Flask(__name__)
@@ -8,25 +7,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'something-secret'
 app.config['SIMPLELOGIN_USERNAME'] = 'chuck'
 app.config['SIMPLELOGIN_PASSWORD'] = 'norris'
-SimpleLogin(app)
+
 
 @app.route('/vox')
-#@login_required
+# @login_required
 def my_form():
-    return render_template('index.html', phrases = vox.getcachedsentences())
+    return render_template('index.html', phrases=vox.getcachedsentences())
 
 
 @app.route('/vox', methods=['POST'])
-#@login_required
+# @login_required
 def my_form_post():
     text = request.form['text']
     processed_text = text.upper()
 
     sentence_data = vox.savetomp3(processed_text)
-    print("received: " +  str(sentence_data))
+    print("received: " + str(sentence_data))
 
-    return render_template('play.html', mp3_link = sentence_data['path'],
-                                        says = sentence_data['sentence'])
+    return render_template('play.html', mp3_link=sentence_data['path'],
+                           says=sentence_data['sentence'])
+
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
